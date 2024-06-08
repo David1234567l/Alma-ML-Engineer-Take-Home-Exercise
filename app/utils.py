2541bb91193bb5040c58835e773b370a28e9d13b
David1234567l <35432315+David1234567l@users.noreply.github.com>
2024-06-08 11:34:35 -0700

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,9 @@
 from openai import OpenAI
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 import os
 from dotenv import load_dotenv
 
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 # Load environment variables from .env file
 load_dotenv()
 
